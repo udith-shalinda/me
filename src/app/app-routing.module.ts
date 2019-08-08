87ee4971e3030f3e205ec515f2040fa5f161a98b
Udith <udithshalinda2@gmail.com>
2019-08-08 20:53:34 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BackgroundComponent } from './background/background.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:BackgroundComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
